96311227b43114e51ef3430e0b7d800f185b5c44
ManideepAnnarapu <ManideepAnnarapu@my.unt.edu>
2024-07-13 18:05:17 -0500

    Logging and exception

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"tarin.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
